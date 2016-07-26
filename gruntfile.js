9cda3cd55a2c36e64388c669d8eaafbf4bd27c5e
Jeffrey Sanford <jeffreysanford@gmail.com>
2016-07-26 21:24:27 +0000

    app setup

--- gruntfile.js
+++ gruntfile.js
@@ -35,8 +35,55 @@ open: {
     all: {
         path: 'http://localhost:8080/index.html'
     }
-}
+  },
+    jsDir: 'public/javascripts/',
+    jsDistDir: 'dist/javascripts/',
+    cssDir: 'public/stylesheets/',
+    cssDistDir: 'dist/stylesheets/',
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      js: {
+        options: {
+          separator: ';'
+        },
+        src: ['<%=jsDir%>*.js'],
+        dest: '<%=jsDistDir%><%= pkg.name %>.js'
+      },
+      css: {
+        src: ['<%=cssDir%>*.css'],
+        dest: '<%=cssDistDir%><%= pkg.name %>.css'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%=grunt.template.today("dd-mm-yyyy") %>
+      },
+      dist: {
+        files: {
+          '<%=jsDistDir%><%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
+        }
+      }
+    },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%=grunt.template.today("dd-mm-yyyy") %> */\n'
+        },
+        files: {
+          '<%=cssDistDir%><%= pkg.name %>.min.css': ['<%= concat.css.dest %>']
+        }
+      }
+    }
 });
+
+
+
+
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
 // Creates the `server` task
 grunt.registerTask('server', [
@@ -45,3 +92,11 @@ grunt.registerTask('server', [
     'watch'
     ]);
 };
+
+//  Create the `default` task
+grunt.registerTask('default', [
+  'concat',
+  'uglify',
+  'cssmin',
+  'watch'
+]
