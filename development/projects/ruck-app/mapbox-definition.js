352829fde74b6d567bcc61ec3cd0280a7361d95c
Jeffrey Sanford <jeffreysanford@gmail.com>
2016-08-07 23:58:33 +0000

    working with mapbox

--- development/projects/ruck-app/mapbox-definition.js
+++ development/projects/ruck-app/mapbox-definition.js
@@ -8,43 +8,38 @@
     var range = document.getElementById('range');
 
     function clip() {
-        var nw = map.containerPointToLayerPoint([0, 0]),
-            se = map.containerPointToLayerPoint(map.getSize()),
-            clipX = nw.x + (se.x - nw.x) * range.value;
+      var nw = map.containerPointToLayerPoint([0, 0]),
+          se = map.containerPointToLayerPoint(map.getSize()),
+          clipX = nw.x + (se.x - nw.x) * range.value;
 
-        overlay.getContainer().style.clip = 'rect(' + [nw.y, clipX, se.y, nw.x].join('px,') + 'px)';
+      overlay.getContainer().style.clip = 'rect(' + [nw.y, clipX, se.y, nw.x].join('px,') + 'px)';
     }
 
     L.geoJson({
-    type: 'FeatureCollection',
-    features: [{
+      type: 'FeatureCollection',
+      features: [{
         type: 'Feature',
         properties: {},
         geometry: {
             type: 'LineString',
             coordinates: [[0, 0], [10, 20]]
         }
-    }, {
+      }, {
         type: 'Feature',
         properties: {},
         geometry: {
             type: 'Point',
             coordinates: [0, 0]
         }
-    }]
+      }]
     }).addTo(map);
     range['oninput' in range ? 'oninput' : 'onchange'] = clip;
-    map.on('move', clip);
     map.on('mousemove', function (e) {
-        document.getElementById('info').innerHTML =
-        // e.point is the x, y coordinates of the mousemove event relative
-        // to the top-left corner of the map
-        JSON.stringify(e.layerPoint.x) + '(x)'+ JSON.stringify(e.layerPoint.y) + '(y)';
-            // e.lngLat is the longitude, latitude geographical position of the event
+        document.getElementById('info').innerHTML = JSON.stringify(e.layerPoint.x) + '(x)'+ JSON.stringify(e.layerPoint.y) + '(y)';
 	document.getElementById('latitude').innerHTML = JSON.stringify(e.latlng.lat.toFixed(2));
 	document.getElementById('longitude').innerHTML = JSON.stringify(e.latlng.lng.toFixed(2));
-
-});
-    map.setView([40.3428, -105.6836], 12);
+    });
+    map.on('move', clip);
+    map.setView([40.3428, -105.6836], 8);
     clip();
 }());
